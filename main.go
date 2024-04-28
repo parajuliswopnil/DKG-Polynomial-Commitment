2ec37e0262ede204b7243c01f1a5a439a621381e
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-04-28 14:38:03 +0545

    backup

--- main.go
+++ main.go
@@ -4,22 +4,20 @@ import "fmt"
 
 var (
 	FiniteFeild = []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	g           = FiniteFeild[2]
-	q           = FiniteFeild[len(FiniteFeild)-1]
-	p           = FiniteFeild[len(FiniteFeild)-1] + 1
+	g           = FiniteFeild[2]                      // 3
+	q           = FiniteFeild[len(FiniteFeild)-1]     // 16
+	p           = FiniteFeild[len(FiniteFeild)-1] + 1 // 17
 )
 
 func main() {
 
-	var i int64
+	var i int64 = 5
 
-	i = 1
-
-	s1 := fx(i) % q
+	s1 := fx(i) 
 
 	commitment1, commitment2, commitment3 := commit()
 
-	gpowsi := Pow(g, s1) % p
+	gpowsi := Pow(g, s1) 
 
 	verify := verify(commitment1, commitment2, commitment3, i)
 	//  when verifying we calclulate g pow si modolus p which should be equal to verify
@@ -43,7 +41,7 @@ func commit() (int64, int64, int64) {
 
 func verify(c1, c2, c3, i int64) int64 {
 
-	return (Pow(c1, Pow(i, 2)) * Pow(c2, Pow(i, 1)) * Pow(c3, Pow(i, 0))) % p
+	return (Pow(c1, Pow(i, 2)) * Pow(c2, Pow(i, 1)) * Pow(c3, Pow(i, 0))) 
 }
 
 func Pow(base, exponenet int64) int64 {
